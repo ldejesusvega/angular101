7635964b71067f6716510230e85eac82a07b84f8
ldejesusvega <ldejesusvega@gmail.com>
2021-05-12 11:50:42 -0500

    Define First component. define property and set value

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
-})
 export class AppComponent {
+  /**
+ * This Component cas be contruct like this ....
+ * 
+ *   constructor() {
+    this.groceries = [];
+  }
+  But in this case usign type script this will change to ...
+ **/
 
+  title: string; // declaring property then...
+
+  constructor() {
+    this.title = "Ultimate Angular"; /// Set property value.
+  }
 }
