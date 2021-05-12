86a7deb13ac06e6f061d8f84cf3d89b91b83dfc3
ldejesusvega <ldejesusvega@gmail.com>
2021-05-12 14:20:55 -0500

    Interpolation Examples

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,18 @@ import { Component } from "@angular/core";
     <div class="app">
       {{ title }}
     </div>
+    <h4>Interpolation Examples</h4>
+    <div>NumberOne is {{ numberOne }}</div>
+    <div>NumberTwo is {{ numberTwo }}</div>
+
+    <h4>Interpolation Calculations</h4>
+    <div>Sum numbers is {{ numberOne + numberTwo }}</div>
+
+    <h4>Interpolation with Boolean Values</h4>
+    <div>is Happy??? {{ isHappy ? ":)" : ":(" }}</div>
+
+    <h4>Concat expression</h4>
+    {{ title + " !!!" }}
   `,
 }) // templateUrl: "./app.component.html", // external template definition
 export class AppComponent {
@@ -20,6 +32,9 @@ export class AppComponent {
  **/
 
   title: string; // declaring property then...
+  isHappy: boolean = false;
+  numberOne: number = 1;
+  numberTwo: number = 2;
 
   constructor() {
     this.title = "Ultimate Angular"; /// Set property value.
