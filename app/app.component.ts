9c9a8c258338a99f7574f4d385388e4bd5d1e13e
ldejesusvega <ldejesusvega@gmail.com>
2021-05-12 14:46:44 -0500

    execise of one way property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,38 +5,39 @@ import { Component } from "@angular/core";
   styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      {{ title }}
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }}</h1>
+      <img [src]="logo" />
+      <br />
+      <input type="text" value="Todd" />
+      <input type="text" value="{{ title }}" />
+      <input type="text" [value]="name" />
     </div>
-    <h4>Interpolation Examples</h4>
-    <div>NumberOne is {{ numberOne }}</div>
-    <div>NumberTwo is {{ numberTwo }}</div>
-
-    <h4>Interpolation Calculations</h4>
-    <div>Sum numbers is {{ numberOne + numberTwo }}</div>
-
-    <h4>Interpolation with Boolean Values</h4>
-    <div>is Happy??? {{ isHappy ? ":)" : ":(" }}</div>
-
-    <h4>Concat expression</h4>
-    {{ title + " !!!" }}
-  `,
-}) // templateUrl: "./app.component.html", // external template definition
+  `, // example of sugar notation <h1 [innerHTML]="title"></h1>
+})
+// Property binding
 export class AppComponent {
-  /**
- * This Component cas be contruct like this ....
- * 
- *   constructor() {
-    this.groceries = [];
-  }
-  But in this case usign type script this will change to ...
- **/
-
-  title: string; // declaring property then...
+  title: string;
   isHappy: boolean = false;
   numberOne: number = 1;
   numberTwo: number = 2;
-
+  logo: string = "img/logo.svg";
+  name: string = "Todd";
   constructor() {
-    this.title = "Ultimate Angular"; /// Set property value.
+    this.title = "Ultimate Angular";
   }
 }
+
+/***
+ *  <input type="text" value="Todd" />
+    <input type="text" value="{{ title }}" />
+ * This two lines shows input with value Todd,
+ * but in second line is binding with {{ title }}
+ * To do this dynamic binding should do it like this 
+ * 
+ * Create the property the bind on input with new property 
+ *  name: string = "Todd";
+ *  <input type="text" [value]="name" />
+ * This is one way databinding 
+ * 
+ */
