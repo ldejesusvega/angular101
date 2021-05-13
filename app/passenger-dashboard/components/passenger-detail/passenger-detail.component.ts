cb0bbc775e668317875aaad486b2e34f02e6a22d
ldejesusvega <ldejesusvega@gmail.com>
2021-05-13 18:50:59 -0500

    add passenger detail in component

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,9 +1,26 @@
-import { Component } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
+import { Passenger } from "../../modules/passenger.interface";
 @Component({
   selector: "passenger-detail",
-  template: ` <div>Detail Component</div> `,
+  styleUrls: ["passenger-detail.component.scss"],
+  template: `
+    <div>
+      <span class="status" [class.checked-in]="detail.checkedIn"></span>
+      {{ detail.fullname }}
+      <div class="date">
+        Checked In Date :
+        {{
+          detail.checkInDate
+            ? (detail.checkInDate | date: "yMMMMd" | uppercase)
+            : "Not Checked Id"
+        }}
+      </div>
+      <div class="children">Children: {{ detail.children?.length || 0 }}</div>
+    </div>
+  `,
 })
 export class PassengerDetailComponent {
+  @Input()
+  detail: Passenger;
   constructor() {}
 }
