1279d3df3a47ad82313ee700982fbe71b751e579
ldejesusvega <ldejesusvega@gmail.com>
2021-05-13 16:48:06 -0500

    import Count and Detail components in dashboard module

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
+// Containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+
+// Conponents
+import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
+import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
 @NgModule({
-  declarations: [PassengerDashboardComponent],
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent,
+  ],
   imports: [CommonModule],
   exports: [PassengerDashboardComponent],
 })
