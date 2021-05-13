0af89c1abc00e78add9c39c067408cec0480ec3e
ldejesusvega <ldejesusvega@gmail.com>
2021-05-13 11:51:08 -0500

    Pipes use examples

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,81 +1,52 @@
 import { Component } from "@angular/core";
+import { Timestamp } from "rxjs";
 
 interface Passenger {
   id: number;
   fullname: string;
   checkedIn: boolean;
+  checkInDate?: number;
 }
+/***
+ *   checkInDate: number | null;
+ * This define checkInDate as number that can be null
+ *
+ * checkInDate?: number ;
+ * Using this prevent error if the api si not returning this property
+ *
+ */
+
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
       <h3>Airline Passenger</h3>
-      <h4>Template definition</h4>
-      <ul>
-        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
-          <li>{{ i }} -- {{ passenger.id }}, {{ passenger.fullname }}</li>
-        </template>
-      </ul>
-      <br />
-
-      <h4>use of class</h4>
-      <h5>this works when_ only require_ add one class</h5>
+      <h4>Pipes, data_ transformation</h4>
+      <h5>pipes procesa datos para ser presentados</h5>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index">
           <span class="status" [class.checked-in]="passenger.checkedIn"></span>
           {{ i }} -- {{ passenger.id }}, {{ passenger.fullname }}
-        </li>
-      </ul>
+          <p>{{ passenger | json }}</p>
 
-      <br />
-      <h4>use of ngclass</h4>
-      <h5>this works when_ only require_ add multiple classes</h5>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [ngClass]="{
-              'checked-in': passenger.checkedIn,
-              'checked-out': !passenger.checkedIn
-            }"
-          ></span>
-          {{ i }} -- {{ passenger.id }}, {{ passenger.fullname }}
-        </li>
-      </ul>
-
-      <br />
-      <h4>use of style</h4>
-      <h5>modify style definition</h5>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [style.backgroundColor]="
-              passenger.checkedIn ? '#2ecc71' : '#c0392b'
-            "
-          ></span>
-          {{ i }} -- {{ passenger.id }}, {{ passenger.fullname }}
-        </li>
-      </ul>
-
-      <br />
-      <h4>use of ngstyle</h4>
-      <h5>modify style definition</h5>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [ngStyle]="{
-              backgroundColor: passenger.checkedIn ? '#2ecc71' : '#c0392b'
-            }"
-          ></span>
-          {{ i }} -- {{ passenger.id }}, {{ passenger.fullname }}
+          <div class="date">
+            Checked In Date :
+            {{
+              passenger.checkInDate
+                ? (passenger.checkInDate | date: "yMMMMd" | uppercase)
+                : "Not Checked Id"
+            }}
+          </div>
         </li>
       </ul>
     </div>
   `,
 })
+/**
+ * <!--build in functions json devuelve el objeto en formato json -->
+ * <!-- build in functions uppercase devuelve la cadena en mayusculas -->
+ */
 
 // Property binding
 export class AppComponent {
@@ -94,31 +65,37 @@ export class AppComponent {
       id: 1,
       fullname: "Stephen",
       checkedIn: true,
+      checkInDate: 1620723600000,
     },
     {
       id: 2,
       fullname: "Rose",
-      checkedIn: false,
+      checkedIn: true,
+      checkInDate: 1620727200000,
     },
     {
       id: 3,
       fullname: "James",
       checkedIn: false,
+      checkInDate: null,
     },
     {
       id: 4,
       fullname: "Thelma",
       checkedIn: true,
+      checkInDate: 1620734400000,
     },
     {
       id: 5,
       fullname: "Louis",
       checkedIn: false,
+      checkInDate: null,
     },
     {
       id: 6,
       fullname: "Enrique",
       checkedIn: true,
+      checkInDate: 1620648000000,
     },
   ];
 }
