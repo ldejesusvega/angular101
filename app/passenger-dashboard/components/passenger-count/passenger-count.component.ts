3d33e82b78b48bffdfd6d46eabe576634093a2c2
ldejesusvega <ldejesusvega@gmail.com>
2021-05-13 16:45:05 -0500

    Create passenger count and detail components

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "passenger-count",
+  template: ` <div>Count Component</div> `,
+})
+export class PassengerCountComponent {
+  constructor() {}
+}
