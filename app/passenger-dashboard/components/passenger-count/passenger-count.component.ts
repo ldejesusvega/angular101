168318d7696185b4c485c114726325a5f3f28795
ldejesusvega <ldejesusvega@gmail.com>
2021-05-13 17:42:21 -0500

    pass data to dummy component

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,9 +1,26 @@
-import { Component } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
+import { Passenger } from "../../modules/passenger.interface";
 @Component({
   selector: "passenger-count",
-  template: ` <div>Count Component</div> `,
+  template: `
+    <div>
+      <h3>Airline Passenger</h3>
+      <div>Total checked in : {{ checkedInCounts() }} / {{ items.length }}</div>
+    </div>
+  `,
 })
 export class PassengerCountComponent {
-  constructor() {}
+  @Input()
+  items: Passenger[];
+
+  checkedInCounts(): number {
+    if (!this.items) return; // Validate if items is not null
+
+    // return this.items.filter((passenger: Passenger) => {
+    //   return passenger.checkedIn;
+    // }).length;
+    // This two segments return the same
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn)
+      .length;
+  }
 }
