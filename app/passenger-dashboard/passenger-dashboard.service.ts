f0dc422db5214518fad0c048f03eb2cdfd02cf83
ldejesusvega <ldejesusvega@gmail.com>
2021-05-14 20:11:00 -0500

    Change approach in services from map to promise

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
+import "rxjs/add/operator/toPromise"; /// Use Promise alternative instead of map
 import { Passenger } from "./modules/passenger.interface";
 
 const PASSENGER_API: string = "/api/passengers";
@@ -15,10 +15,11 @@ export class PassengerDashboardService {
    * this services get all passengers
    * @returns Passenger[]
    */
-  getPassengers(): Observable<Passenger[]> {
+  getPassengers(): Promise<Passenger[]> {
     return this.http
       .get(PASSENGER_API)
-      .map((response: Response) => response.json());
+      .toPromise()
+      .then((response: Response) => response.json());
   }
 
   /**
@@ -26,7 +27,7 @@ export class PassengerDashboardService {
    * @param passenger Type of Passenger
    * @returns json object
    */
-  updatePassenger(passenger: Passenger): Observable<Passenger> {
+  updatePassenger(passenger: Passenger): Promise<Passenger> {
     let headers = new Headers({
       "Content-Type": "application/json",
     });
@@ -35,7 +36,8 @@ export class PassengerDashboardService {
     });
     return this.http
       .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
-      .map((response: Response) => response.json());
+      .toPromise()
+      .then((response: Response) => response.json());
   }
 
   /**
@@ -44,9 +46,10 @@ export class PassengerDashboardService {
    * @param passenger Type of passenger
    * @returns Json Object
    */
-  removePassenger(passenger: Passenger): Observable<Passenger> {
+  removePassenger(passenger: Passenger): Promise<Passenger> {
     return this.http
       .delete(`${PASSENGER_API}/${passenger.id}`)
-      .map((response: Response) => response.json());
+      .toPromise()
+      .then((response: Response) => response.json());
   }
 }
