3372317a22b7b8753018c357e647ccd00a0fec5b
ldejesusvega <ldejesusvega@gmail.com>
2021-05-17 10:10:54 -0500

    Import Passenger Viewer component... Change component to export

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from "@angular/http";
 
 // Containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerViewerComponent } from "./containers/passenger-viewer/passenger-viewer.component";
 
 // Conponents
 import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
@@ -15,11 +16,12 @@ import { PassengerDashboardService } from "./passenger-dashboard.service";
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
+    PassengerViewerComponent,
     PassengerCountComponent,
     PassengerDetailComponent,
   ],
   imports: [CommonModule, HttpModule],
-  exports: [PassengerDashboardComponent],
+  exports: [PassengerViewerComponent],
   providers: [PassengerDashboardService],
 })
 export class PassengerDashboardModule {}
