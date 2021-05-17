4e2e5f8a4241011c59a4d276005d1f939875dd23
ldejesusvega <ldejesusvega@gmail.com>
2021-05-17 18:21:36 -0500

    Add ngSubmit to form Add output parameter to handle event Add handleSubmit method

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { Passenger } from "../../modules/passenger.interface";
 import { Baggage } from "../../modules/baggage.interface";
 
@@ -6,7 +6,11 @@ import { Baggage } from "../../modules/baggage.interface";
   selector: "passenger-form",
   styleUrls: ["passenger-form.component.scss"],
   template: `
-    <form #form="ngForm" novalidate>
+    <form
+      (ngSubmit)="handleSubmit(form.value, form.valid)"
+      #form="ngForm"
+      novalidate
+    >
       <div>
         Passenger Fullname :
         <input
@@ -74,6 +78,9 @@ export class PassengerFormComponent {
   @Input()
   detail: Passenger;
 
+  @Output()
+  update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
   baggage: Baggage[] = [
     {
       key: "none",
@@ -96,6 +103,12 @@ export class PassengerFormComponent {
   toggleChechIk(checkedIn: boolean) {
     if (checkedIn) {
       this.detail.checkInDate = Date.now();
+    }
+  }
+
+  handleSubmit(passenger: Passenger, isValid: boolean) {
+    if (isValid) {
+      this.update.emit(passenger);
     }
   }
 }
