3007a924535703e028fc905b08f5d1ece319d08a
ldejesusvega <ldejesusvega@gmail.com>
2021-05-17 21:45:46 -0500

    add PassengerDashboardModule to navigation menu

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from "@angular/core";
+import { NgModule, Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
 // Containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
@@ -15,6 +16,13 @@ import { PassengerFormComponent } from "./components/passenger-form/passenger-fo
 // services
 import { PassengerDashboardService } from "./passenger-dashboard.service";
 
+const routes: Routes = [
+  {
+    path: "passengers",
+    component: PassengerDashboardComponent,
+  },
+];
+
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
@@ -23,8 +31,15 @@ import { PassengerDashboardService } from "./passenger-dashboard.service";
     PassengerDetailComponent,
     PassengerFormComponent,
   ],
-  imports: [CommonModule, HttpModule, FormsModule],
-  exports: [PassengerViewerComponent],
+  imports: [
+    CommonModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+  ],
+
   providers: [PassengerDashboardService],
 })
 export class PassengerDashboardModule {}
+
+//  exports: [PassengerViewerComponent],
