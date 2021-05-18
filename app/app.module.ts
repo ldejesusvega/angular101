5f3f10cda690ab31b949eb108c9110853e3e8253
ldejesusvega <ldejesusvega@gmail.com>
2021-05-17 20:10:55 -0500

    Add Not found component to app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,13 +5,15 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 import { HomeComponent } from "./home.component";
+import { NotFoundComponent } from "./not-found.component";
 import { AppComponent } from "./app.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "**", component: NotFoundComponent },
 ];
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     CommonModule,
