86e27dabedfc755d5446833ced23249b184d0718
ldejesusvega <ldejesusvega@gmail.com>
2021-05-18 13:55:28 -0500

    implement usehash support in url

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     CommonModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
     PassengerDashboardModule,
   ],
   bootstrap: [AppComponent],
