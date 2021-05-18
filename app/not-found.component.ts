3240d5f3cf0a4c98e1b20d67c59bd35320a4906b
ldejesusvega <ldejesusvega@gmail.com>
2021-05-17 20:45:04 -0500

    add routerLink in not found to go home

--- app/not-found.component.ts
+++ app/not-found.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 @Component({
   selector: "not-found",
-  template: ` <div>Not Found!!!</div> `,
+  template: ` <div>Not Found!!!, <a routerLink="/">go home</a>?</div> `,
 })
 export class NotFoundComponent {}
